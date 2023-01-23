b26a9c9f7b1edae28f016e834a978ab7a9c62cc5
Peter Crowther <13117069+merrygoat@users.noreply.github.com>
2023-01-23 20:42:55 +0000

    List both direct and group policies of IAM users

--- access_audit.py
+++ access_audit.py
@@ -3,22 +3,22 @@ import botocore.client
 from typing import Type
 
 
-def print_iam_report(account_details: dict[str: list]):
+def print_iam_report(account_details: dict[str: dict]):
     # Unpack policies from list into dict
-    account_details["Policies"] = {policy["PolicyName"]: policy for policy in account_details["Policies"]}
 
     with open("iam.txt", 'w') as output_file:
         policy_list = []
-        output_file.write("Users, assigned groups and attached managed policies.\n\n")
-        for user in account_details["UserDetailList"]:
-            attached_managed_policies = [policy["PolicyName"] for policy in user["AttachedManagedPolicies"]]
-            policy_list.extend(attached_managed_policies)
-            output_file.write(f"{user['UserName']} - {', '.join(user['GroupList'])} - {', '.join(attached_managed_policies)}\n")
+        output_file.write("Users and policies.\n\n")
+        for username, user_details in account_details["Users"].items():
+            attached_policies, group_policies = get_users_policies(user_details, account_details["Groups"])
+            attached_policies = [f"{policy} (a)" for policy in attached_policies]
+            group_policies = [f"{policy} (g)" for policy in group_policies]
+            output_file.write(f"{username} - {', '.join(group_policies)}, {', '.join(attached_policies)}\n")
         output_file.write("\nGroups and attached policies\n\n")
-        for group in account_details["GroupDetailList"]:
-            group_policy_names = [policy['PolicyName'] for policy in group['AttachedManagedPolicies']]
+        for group_name, group_details in account_details["Groups"].items():
+            group_policy_names = [policy['PolicyName'] for policy in group_details['AttachedManagedPolicies']]
             policy_list.extend(group_policy_names)
-            output_file.write(f"{group['GroupName']} - {', '.join(group_policy_names)}\n")
+            output_file.write(f"{group_name} - {', '.join(group_policy_names)}\n")
         output_file.write("\nPolicy Details\n\n")
         for policy in set(policy_list):
             if policy in account_details["Policies"]:
@@ -27,7 +27,17 @@ def print_iam_report(account_details: dict[str: list]):
                 output_file.write(f"{policy} - AWS Managed\n")
 
 
-def get_account_details(iam_client: Type[botocore.client.BaseClient]) -> dict[str: list]:
+def get_users_policies(user_details: dict, group_details: dict) -> tuple[list[str], list[str]]:
+    """Return the policies directly attached to a user and the policies attached to groups the user is in."""
+    attached_managed_policies = [policy["PolicyName"] for policy in user_details["AttachedManagedPolicies"]]
+    user_groups = user_details["GroupList"]
+    group_policies = []
+    for group_name in user_groups:
+        group_policies.extend([policy['PolicyName'] for policy in group_details[group_name]['AttachedManagedPolicies']])
+    return attached_managed_policies, group_policies
+
+
+def get_account_details(iam_client: Type[botocore.client.BaseClient]) -> dict[str: dict]:
     details = {"UserDetailList": [], "GroupDetailList": [], "RoleDetailList": [], "Policies": []}
     # noinspection PyArgumentList
     paginator = iam_client.get_paginator('get_account_authorization_details')
@@ -35,6 +45,12 @@ def get_account_details(iam_client: Type[botocore.client.BaseClient]) -> dict[st
     for page in page_iterator:
         for item in details:
             details[item].extend(page[item])
+    # Unpack lists of items into dicts
+    details["Users"] = {user["UserName"]: user for user in details.pop("UserDetailList")}
+    details["Groups"] = {group["GroupName"]: group for group in details.pop("GroupDetailList")}
+    details["Roles"] = {role["RoleName"]: role for role in details.pop("RoleDetailList")}
+    details["Policies"] = {policy["PolicyName"]: policy for policy in details.pop("Policies")}
+
     return details
 
 
